3a9d18e2a376880d1358d2fb6d04789f275d5075
Bondulance <bodhiw417@gmail.com>
2025-03-05 22:55:19 -0500

    almost done ish

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,13 +1,50 @@
+// all of my variables
 const btn = document.getElementById("mobileButton");
 const menu = document.getElementById("hamburgerMenu");
 const x = document.getElementById("close");
-console.log(btn);
 
+// variables for image carousel on Architecture.html
+const btn1 = document.getElementById("shard");
+const btn2 = document.getElementById("pauls");
+const btn3 = document.getElementById("gherkin");
+const imageSpace = document.getElementById("carouselImg");
+
+console.log(btn);
+// this is for my hamburger menu
 btn.addEventListener("click", function() {
     console.log("btn clicked");
+    // makes the div display flex, differently from its none default
     menu.style.display = "flex";
 });
 
 x.addEventListener("click", function() {
+    // makes it go away
     menu.style.display = "none";
-})
+});
+
+
+btn1.addEventListener("click", function() {
+    console.log("btn1 clicked: image displayed is Shard");
+    const value = btn1.dataset.img;
+    console.log(value);
+    changeCarousel(value);
+});
+
+btn2.addEventListener("click", function() {
+    console.log("btn2 clicked: image displayed is St Pauls");
+    const value = btn2.dataset.img;
+    console.log(value);
+    changeCarousel(value);
+});
+
+btn3.addEventListener("click", function() {
+    console.log("btn3 clicked: image displayed is Gherkin");
+    const value = btn3.dataset.img;
+    console.log(value);
+    changeCarousel(value);
+});
+
+function changeCarousel(path) {
+    imageSpace.src = path;
+    console.log("Image changed");
+}
