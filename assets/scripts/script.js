bd995e02acf76c03db0e8121da4688c4235e643e
Bondulance <bodhiw417@gmail.com>
2025-03-05 20:06:52 -0500

    hamburger menu

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,1 +1,13 @@
-console.log("Push this to git")
+const btn = document.getElementById("mobileButton");
+const menu = document.getElementById("hamburgerMenu");
+const x = document.getElementById("close");
+console.log(btn);
+
+btn.addEventListener("click", function() {
+    console.log("btn clicked");
+    menu.style.display = "flex";
+});
+
+x.addEventListener("click", function() {
+    menu.style.display = "none";
+})
